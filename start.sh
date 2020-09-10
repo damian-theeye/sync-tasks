60ad65e3a2a2f4306070d23cdbf7d8a677f030f7
Javier Ailbirt <jailbirt@gmail.com>
2020-09-09 21:25:22 -0300

    adding region

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "fetching config files"
-aws s3 cp s3://theeye-internal/production/config/sync-tasks.js ./server/config/index.js
+aws s3 cp s3://theeye-internal/production/config/sync-tasks.js ./server/config/index.js --region us-east-1
 echo "validating..."
 ls ./server/config/
 npm run start
