afc143a4296039937a185dd5e8ead997ae5f0aaa
facugon <facugon@theeye.io>
2020-09-10 11:26:38 -0300

    added debug

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "fetching config files"
 aws s3 cp s3://theeye-internal/production/config/sync-tasks.js ./server/config/index.js --region us-east-1
+export DEBUG="*eye*"
 npm run start
