7ecf6eecd0acd1ff24f496016cf7fcde1349a020
Javier Ailbirt <jailbirt@gmail.com>
2020-09-09 22:21:12 -0300

    add role to task

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "debuging aws"
-curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
 echo "fetching config files"
 aws s3 cp s3://theeye-internal/production/config/sync-tasks.js ./server/config/index.js --region us-east-1
-echo "validating..."
-ls ./server/config/
 npm run start
